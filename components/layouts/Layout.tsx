e233c3c1b6e8ac7c954dd587eed0e0360f9b5e3b
Salvador Sánchez <salsanzmelc@gmail.com>
2023-06-28 19:21:03 -0600

    open jira antes de subir a producción y arreglos finales

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,18 +8,18 @@ interface Props {
   children: React.ReactNode
 }
 
-export const Layout:FC<Props> = ({ title = 'OpenJira app', children }) => {
+export const Layout: FC<Props> = ({ title = 'OpenJira app', children }) => {
   return (
-      <Box sx={{ flexflow: 1 }}>
-        <Head>
-          <title>{ title }</title>
-        </Head>
-        <Navbar />
-        <Sidebar />  
-        <Box sx={{ paddingTop: '10px 20px' }}>
-          { children }
-        </Box>
+    <Box sx={{ flexflow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navbar />
+      <Sidebar />
+      <Box sx={{ paddingTop: '10px 20px' }}>
+        {children}
       </Box>
-      )
+    </Box>
+  )
 }
 
